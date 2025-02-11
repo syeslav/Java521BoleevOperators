1cb018375e9b1af77f720733473a15c6cf47a088
ruslanboleev <ruslanboleev007@gmail.com>
2025-02-11 22:20:32 +0700

    four

---
+++ src/TaskFour.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TaskFour {
+    public static void main(String[] args) {
+        // Todo: Напишите программу, которая выводит таблицу умножения для числа, введенного пользователем.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int b = number * i;
+            System.out.println(number + " x " + i + " = " + b);
+
+            }
+            //System.out.println(number);
+
+        }
+
+    }
